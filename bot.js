195c4dbcc49a446940f85197be07de3af00c4604
TBolton2000 <52944725+TBolton2000@users.noreply.github.com>
2020-01-25 17:26:20 -0600

    changed bot and server

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const ACCESS_TOKEN = 'NyJMui4CjBxQy9gUKK2lgca227DWsztrc97yzwGv';
-var USER_ID = "81946993";
+
+
+
+let USER_ID = "81946993";
 
 var API = require('groupme').Stateless;
 
@@ -7,12 +10,23 @@ var IStream = require('groupme').IncomingStream;
 
 API.Users.me(ACCESS_TOKEN, (err,ret) => {
     if (!err) {
-      USER_ID = ret.id;
-      console.log(USER_ID);
+        USER_ID = ret.id;
+        console.log(USER_ID);
     }
-  });
+});
+
+
+
 
 var listener = new IStream(ACCESS_TOKEN, USER_ID);
+
+var DMResponse = function(msg) {
+    //Respond accordingly
+};
+
+var GroupMessageResponse = function(msg) {
+    //SAVE INFO TO DATA BASE
+}
 
 listener.on("connected", function() {
     console.log("Connected Successfully.");
@@ -21,7 +35,10 @@ listener.on("connected", function() {
 //Listener for incoming messages
 listener.on("message", function(msg) {
     if(msg && msg.data && msg.data.subject && msg.data.subject.text) {
-        console.log("Message received:", msg.data.subject.text);
+        if (msg.data) {
+            console.log(msg);
+        }
+        console.log("Message received:", msg);
     }
 });
 
