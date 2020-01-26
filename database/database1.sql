6244942b28f015bb6fc87ec20bf6d07f3ded6a3d
neel2520 <55898656+neel2520@users.noreply.github.com>
2020-01-25 20:49:59 -0600

    Delete database1.sql

--- database/database1.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE tamuhack_database;
-CREATE TABLE tamuhack_database.collection
-(
-    ID int NOT NULL,
-    UID varchar(255) NOT NULL,
-    Name varchar(255) NOT NULL,
-    PRIMARY KEY(ID)
-);
