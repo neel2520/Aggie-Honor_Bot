be23cc1f00f393e1c6d315a413e6d142dfa43934
neel2520 <55898656+neel2520@users.noreply.github.com>
2020-01-25 18:22:59 -0600

    Add files via upload

---
+++ database/database1.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE tamuhack_database;
+CREATE TABLE tamuhack_database.collection
+(
+    ID int NOT NULL,
+    UID varchar(255) NOT NULL,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY(ID)
+);
